a42bf4c005bd93a5c432b40fbc769807a38ce842
dlundgaard <dlundgaard8@gmail.com>
2021-02-12 17:51:32 +0100

    converted actions to structs with an associated callback to avoid code duplication

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fmt;
 use std::process::{
     exit, 
     Command,
@@ -12,189 +13,149 @@ use console::Term;
 // TODO
 // remove all println's and use Term.write_line
 // error handling
+// figure out getting colored terminal output when collecting from git status
 
-fn main() {
-    let git_repo = GitRepo::new();
-
-	loop {
-		let actions = [
-			"status",
-			"stage",
-			"unstage",
-			"commit",
-			"push",
-			"pull",
-			"exit",
-		];
-		let selected = Select::new()
-			.with_prompt("What do you want to do?")
-			.items(&actions[..])
-			.default(0)
-			.interact()
-			.unwrap();
-		let selected_action = actions[selected];
-		match selected_action {
-		   "status"     => git_repo.print_status(),
-		   "stage"      => git_repo.staging_mode(),
-		   "unstage"    => git_repo.unstaging_mode(),
-		   "commit"     => git_repo.do_commit(),
-		   "push"       => git_repo.do_push(),
-		   "pull"       => git_repo.do_pull(),
-		   "exit"       => { println!("\nThanks for stopping by!"); exit(0) },
-		   _            => panic!("Unknown action"),
-		}
-	}
+fn get_git_root_dir() -> String {
+    let command_output = Command::new("git")
+        .args(&["rev-parse", "--show-toplevel"])
+        .output()
+        .expect("git command failed");
+    let mut output_as_string = String::from_utf8_lossy(&command_output.stdout).to_string();
+    output_as_string.pop(); // remove trailing newline from output
+    output_as_string.to_owned()
 }
 
-struct GitRepo {
-    repo_root_dir: String,
+fn get_changed_files() -> Vec<ProjectFile> {
+    let command_output = Command::new("git")
+        .args(&["status", "--porcelain"])
+        .output()
+        .expect("git command failed");
+    let output_as_string = String::from_utf8_lossy(&command_output.stdout);
+    let lines: Vec<&str> = output_as_string.split("\n").filter(|&s| !s.is_empty()).collect();
+    lines.into_iter().map(ProjectFile::from_line).collect()
 }
 
-impl GitRepo {
-    fn new() -> GitRepo {
-        let command_output = Command::new("git")
-            .args(&["rev-parse", "--show-toplevel"])
-            .output()
-            .expect("git command failed");
-        let mut output_as_string = String::from_utf8_lossy(&command_output.stdout).to_string();
-        output_as_string.pop(); // remove trailing newline from output
-        GitRepo { repo_root_dir: output_as_string.to_owned() }
-    }
+fn get_staged_files() -> Vec<ProjectFile> {
+    get_changed_files().into_iter()
+        .filter(ProjectFile::is_staged)
+        .collect()
+}
 
-    fn get_changed_files(&self) -> Vec<ProjectFile> {
-        let command_output = Command::new("git")
-            .args(&["status", "--porcelain"])
-            .output()
-            .expect("git command failed");
-        let output_as_string = String::from_utf8_lossy(&command_output.stdout);
-        let lines: Vec<&str> = output_as_string.split("\n").filter(|&s| !s.is_empty()).collect();
-        lines.into_iter().map(ProjectFile::from_line).collect()
-    }
+fn get_not_staged_files() -> Vec<ProjectFile> {
+    get_changed_files().into_iter()
+        .filter(ProjectFile::is_not_staged)
+        .collect()
+}
 
-    fn get_staged_files(&self) -> Vec<ProjectFile> {
-        self.get_changed_files().into_iter()
-            .filter(ProjectFile::is_staged)
-            .collect()
-    }
+fn clear_lines(amount_lines_to_clear: usize) {
+    Term::stdout().clear_last_lines(amount_lines_to_clear).unwrap();
+}
 
-    fn get_not_staged_files(&self) -> Vec<ProjectFile> {
-        self.get_changed_files().into_iter()
-            .filter(ProjectFile::is_not_staged)
-            .collect()
-    }
+fn confirm_return() {
+    Term::stdout().write_str("\nPress any key to go back ").unwrap();
+    Term::stdout().read_key().unwrap();
+    Term::stdout().write_line("").unwrap();
+    clear_lines(2);
+}
 
-    fn clear_lines(&self, amount_lines_to_clear: usize) {
-        Term::stdout().clear_last_lines(amount_lines_to_clear).unwrap();
-    }
+fn print_status() {
+    let command_output = Command::new("git")
+        .args(&["status"])
+        .output()
+        .expect("git command failed");
+    let output_as_string = String::from_utf8_lossy(&command_output.stdout);
+    let amount_lines = output_as_string.matches("\n").count();
+    Term::stdout().write_str(&output_as_string).unwrap();
+    confirm_return();
+    clear_lines(amount_lines);
+}
 
-    fn confirm_return(&self) {
-        Term::stdout().write_str("\nPress any key to go back ").unwrap();
-        Term::stdout().read_key().unwrap();
-        Term::stdout().write_line("").unwrap();
-        self.clear_lines(2);
-    }
-
-    fn print_status(&self) {
-        let command_output = Command::new("git")
-            .args(&["status"])
-            .current_dir(&self.repo_root_dir)
-            .output()
-            .expect("git command failed");
-        let output_as_string = String::from_utf8_lossy(&command_output.stdout);
-        let amount_lines = output_as_string.matches("\n").count();
-        Term::stdout().write_str(&output_as_string).unwrap();
-        self.confirm_return();
-        self.clear_lines(amount_lines);
-    }
-
-    fn staging_mode(&self) {
-        let unstaged_files = self.get_not_staged_files();
-        let unstaged_files_names: Vec<String> = unstaged_files.into_iter().map(|pf| pf.file_path).collect();
-        let mut amount_lines = 0;
-        if !unstaged_files_names.is_empty() { 
-            let selections = MultiSelect::new()
-                .with_prompt("Which files should be staged?")
-                .items(&unstaged_files_names[..])
-                .interact()
-                .unwrap();
-            amount_lines += selections.len() + 1;
-            for selected in selections {
-                let command_output = Command::new("git")
-                    .args(&["add", &unstaged_files_names[selected]])
-                    .current_dir(&self.repo_root_dir)
-                    .output()
-                    .expect("git command failed");
-                let output_as_string = String::from_utf8_lossy(&command_output.stdout);
-                println!("out {:?}", command_output);
-                amount_lines += output_as_string.matches("\n").count();
-                Term::stdout().write_str(&output_as_string).unwrap();
-                println!("{} added to staging area", unstaged_files_names[selected]);
-            }
-        } else {
-            println!("No unstaged files");
-            amount_lines += 1;
+fn staging_mode(repo_root_dir: &str) {
+    let unstaged_files = get_not_staged_files();
+    let unstaged_files_names: Vec<String> = unstaged_files.into_iter().map(|pf| pf.file_path).collect();
+    let mut amount_lines = 0;
+    if !unstaged_files_names.is_empty() { 
+        let selections = MultiSelect::new()
+            .with_prompt("Which files should be staged?")
+            .items(&unstaged_files_names[..])
+            .interact()
+            .unwrap();
+        amount_lines += selections.len() + 1;
+        for selected in selections {
+            let command_output = Command::new("git")
+                .args(&["add", &unstaged_files_names[selected]])
+                .current_dir(&repo_root_dir)
+                .output()
+                .expect("git command failed");
+            let output_as_string = String::from_utf8_lossy(&command_output.stdout);
+            println!("out {:?}", command_output);
+            amount_lines += output_as_string.matches("\n").count();
+            Term::stdout().write_str(&output_as_string).unwrap();
+            println!("{} added to staging area", unstaged_files_names[selected]);
         }
-        self.clear_lines(amount_lines);
+    } else {
+        println!("No unstaged files");
+        amount_lines += 1;
     }
+    clear_lines(amount_lines);
+}
 
-    fn unstaging_mode(&self) {
-        let staged_files = self.get_staged_files();
-        let staged_files_names: Vec<String> = staged_files.into_iter().map(|pf| pf.file_path).collect();
-        let mut amount_lines = 0;
-        if !staged_files_names.is_empty() { 
-            let selections = MultiSelect::new()
-                .with_prompt("Which files should be unstaged?")
-                .items(&staged_files_names[..])
-                .interact()
-                .unwrap();
-            amount_lines += selections.len() + 1;
-            for selected in selections {
-                let command_output = Command::new("git")
-                    .args(&["reset", &staged_files_names[selected]])
-                    .current_dir(&self.repo_root_dir)
-                    .output()
-                    .expect("git command failed");
-                let output_as_string = String::from_utf8_lossy(&command_output.stdout);
-                amount_lines += output_as_string.matches("\n").count();
-                Term::stdout().write_str(&output_as_string).unwrap();
-                println!("{} removed from staging area", staged_files_names[selected]);
-            }
-        } else {
-            println!("No staged files");
-            amount_lines += 1;
+fn unstaging_mode(repo_root_dir: &str) {
+    let staged_files = get_staged_files();
+    let staged_files_names: Vec<String> = staged_files.into_iter().map(|pf| pf.file_path).collect();
+    let mut amount_lines = 0;
+    if !staged_files_names.is_empty() { 
+        let selections = MultiSelect::new()
+            .with_prompt("Which files should be unstaged?")
+            .items(&staged_files_names[..])
+            .interact()
+            .unwrap();
+        amount_lines += selections.len() + 1;
+        for selected in selections {
+            let command_output = Command::new("git")
+                .args(&["reset", &staged_files_names[selected]])
+                .current_dir(&repo_root_dir)
+                .output()
+                .expect("git command failed");
+            let output_as_string = String::from_utf8_lossy(&command_output.stdout);
+            amount_lines += output_as_string.matches("\n").count();
+            Term::stdout().write_str(&output_as_string).unwrap();
+            println!("{} removed from staging area", staged_files_names[selected]);
         }
-        self.clear_lines(amount_lines);
+    } else {
+        println!("No staged files");
+        amount_lines += 1;
     }
+    clear_lines(amount_lines);
+}
 
-    fn do_commit(&self) {
-        let staged_files = self.get_staged_files();
-        let unstaged_files = self.get_not_staged_files();
-        if staged_files.is_empty() {
-            println!("There are no staged files");
-        } else if unstaged_files.len() > 0 {
-            if Confirm::new()
-                .with_prompt("There are unstaged files. Do you want to continue?")
-                .interact()
-                .unwrap() 
-            { 
-                let command_output = Command::new("git")
-                    .args(&["commit"])
-                    .current_dir(&self.repo_root_dir)
-                    .output()
-                    .expect("git command failed");
-                let output_as_string = String::from_utf8_lossy(&command_output.stdout);
-                println!("{}", output_as_string);
-            }
-        } else {
-            // TODO 
+fn do_commit() {
+    let staged_files = get_staged_files();
+    let unstaged_files = get_not_staged_files();
+    if staged_files.is_empty() {
+        println!("There are no staged files");
+    } else if unstaged_files.len() > 0 {
+        if Confirm::new()
+            .with_prompt("There are unstaged files. Do you want to continue?")
+            .interact()
+            .unwrap() 
+        { 
+            let command_output = Command::new("git")
+                .args(&["commit"])
+                .output()
+                .expect("git command failed");
+            let output_as_string = String::from_utf8_lossy(&command_output.stdout);
+            println!("{}", output_as_string);
         }
+    } else {
+        // TODO 
     }
+}
 
-    fn do_push(&self) {
-    }
+fn do_push() {
+}
 
-    fn do_pull(&self) {
-    }
+fn do_pull() {
 }
 
 #[derive(Debug)]
@@ -243,3 +204,72 @@ impl ProjectFile {
         !self.is_staged()
     }
 }
+
+fn exit_gracefully() {
+    println!("\nThanks for stopping by!"); 
+    exit(0);
+}
+
+struct Action<'a> {
+    name: String,
+    callback: Box<dyn Fn() + 'a>
+}
+
+impl<'a> Action<'a> {
+    fn new<F>(name: &str, callback_closure: F) -> Action<'a>
+    where F: Fn() + 'a {
+        Action {
+            name: String::from(name),
+            callback: Box::new(callback_closure),
+        }
+    }
+
+    fn run_action(&self) {
+        (self.callback)();
+    }
+}
+
+impl fmt::Display for Action<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.name)
+    }
+}
+
+fn main() {
+    let git_root_dir = get_git_root_dir();
+    let mut actions: Vec<Action> = Vec::new();
+
+    actions.push(
+        Action::new("status", || print_status())
+    );
+    actions.push(
+        Action::new("stage", || staging_mode(&git_root_dir))
+    );
+    actions.push(
+        Action::new("unstage", || unstaging_mode(&git_root_dir))
+    );
+    actions.push(
+        Action::new("commit", || do_commit())
+    );
+    actions.push(
+        Action::new("push", || do_push())
+    );
+    actions.push(
+        Action::new("pull", || do_pull())
+    );
+    actions.push(
+        Action::new("exit", || exit_gracefully())
+    );
+
+	loop {
+		let selected = Select::new()
+			.with_prompt("What do you want to do?")
+			.items(&actions)
+			.default(0)
+			.interact()
+			.unwrap();
+        let selected_action = &actions[selected];
+        selected_action.run_action();
+	}
+}
+
