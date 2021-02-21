91c333eabc396c53b50964f876126a9c0487b05e
dlundgaard <dlundgaard8@gmail.com>
2021-02-21 10:55:20 +0100

    added parsing for command line arguments using `clap` to get version and help/usage

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use gitty::modes;
+use clap::App;
 
 fn main() {
+    App::new(env!("CARGO_PKG_NAME"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .about("Interactive command line interface for enhanced workflow when using the Git CLI")
+        .get_matches();
     modes::select_command_mode();
 }
 
